83848c67ab95efec6e78c542290a1a95db6ec372
Chris Powell <cmpowell97@gmail.com>
2023-10-28 20:16:34 -0400

    add system to module

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,5 @@
-import { terser } from 'rollup-plugin-terser'
-import multi from '@rollup/plugin-multi-entry'
+import { terser } from 'rollup-plugin-terser';
+import multi from '@rollup/plugin-multi-entry';
 
 export default [
     {
